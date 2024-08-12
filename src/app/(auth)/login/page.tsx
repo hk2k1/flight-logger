db718cf7d520f8e9fc317bd5352dc31e8dc3c538
hk2k1 <86213550+hk2k1@users.noreply.github.com>
2024-08-12 22:21:56 +0800

    Providers Added
    Auth and Theme providers added
    
    Note: Seems like useSession
    does not work - Workaround use getSessison

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from "@/components/ui/button";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import UserAuthForm from "@/components/forms/user-auth-form";
-import { Plane } from "lucide-react";
+import { Plane, PlaneLanding, PlaneTakeoff } from "lucide-react";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -26,10 +26,11 @@ export default function AuthenticationPage() {
       >
         Login
       </Link>
+
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="absolute inset-0 bg-zinc-900"></div>
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <Plane className="mr-2 h-6 w-6" />
+          <PlaneTakeoff className="mr-2 h-6 w-6" />
           FlightLogger
         </div>
         <div className="relative z-20 mt-auto">
