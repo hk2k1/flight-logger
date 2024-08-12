d5229928ac8e2af0ef6fdb0c405f302cc77473e9
hk2k1 <86213550+hk2k1@users.noreply.github.com>
2024-08-12 22:33:42 +0800

    Header - Frontend
    Added
    mobile sidebar
    user navigation
    avatar shadecn ui component

---
+++ src/components/layout/user-nav.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Session } from "next-auth";
+import { getSession, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+export function UserNav() {
+  // NOTE: This useSession which is from session providers does not seem to work -> shows unauthenticated and need to reload page
+  // Workaround: use this useEffect and getSession instead
+  // refer to -> https://github.com/nextauthjs/next-auth/discussions/5719
+  // const { data: session, status } = useSession();
+  const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getSession();
+      setSession(session);
+      setLoading(false);
+    };
+    fetchSession();
+  }, []);
+  console.log(session);
+
+  const router = useRouter();
+  if (session) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage
+                src={session.user?.image ?? ""}
+                alt={session.user?.name ?? ""}
+              />
+              <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={() => router.push(`/profile`)}>
+              Profile
+              {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => router.push(`/dashboard`)}>
+              FlightLogs
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => signOut()}>
+            Log out
+            {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+}
